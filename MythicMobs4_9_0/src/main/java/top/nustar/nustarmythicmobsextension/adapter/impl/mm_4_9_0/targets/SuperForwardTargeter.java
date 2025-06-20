bc3dd5d183f33c2a5c2704a16fa9db33e07dbaa6
NuStar <3318029085@qq.com>
2025-06-20 23:00:49 +0800

    style(all): 应用代码格式化
    Signed-off-by: NuStar <3318029085@qq.com>

--- MythicMobs4_9_0/src/main/java/top/nustar/nustarmythicmobsextension/adapter/impl/mm_4_9_0/targets/SuperForwardTargeter.java
+++ MythicMobs4_9_0/src/main/java/top/nustar/nustarmythicmobsextension/adapter/impl/mm_4_9_0/targets/SuperForwardTargeter.java
@@ -1,3 +1,21 @@
+/*
+ *    NuStarMythicMobsExtension
+ *    Copyright (C) 2025  NuStar
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.nustar.nustarmythicmobsextension.adapter.impl.mm_4_9_0.targets;
 
 import io.lumine.xikage.mythicmobs.adapters.AbstractLocation;
@@ -7,16 +25,12 @@ import io.lumine.xikage.mythicmobs.skills.SkillMetadata;
 import io.lumine.xikage.mythicmobs.skills.placeholders.parsers.PlaceholderDouble;
 import io.lumine.xikage.mythicmobs.skills.placeholders.parsers.PlaceholderFloat;
 import io.lumine.xikage.mythicmobs.skills.targeters.ILocationSelector;
+import java.util.HashSet;
 import top.nustar.nustarmythicmobsextension.adapter.impl.NuStarTargerSelector;
 
-import java.util.HashSet;
-
 /**
- * @author : NuStar
- * Date : 2025/6/20 21:38
- * Website : <a href="https://www.nustar.top">nustar's web</a>
- * Github : <a href="https://github.com/nustarworld">nustar's github</a>
- * QQ : 3318029085
+ * @author : NuStar Date : 2025/6/20 21:38 Website : <a href="https://www.nustar.top">nustar's web</a> Github : <a
+ *     href="https://github.com/nustarworld">nustar's github</a> QQ : 3318029085
  */
 public class SuperForwardTargeter extends ILocationSelector implements NuStarTargerSelector {
     protected PlaceholderDouble forward;
@@ -29,13 +43,13 @@ public class SuperForwardTargeter extends ILocationSelector implements NuStarTar
 
     public SuperForwardTargeter(MythicLineConfig mlc) {
         super(mlc);
-        this.forward = mlc.getPlaceholderDouble(new String[]{"forward", "f", "amount", "a"}, 5.0D);
-        this.rotate = mlc.getPlaceholderFloat(new String[]{"rotate", "rot"}, 0.0F);
-        this.useEyeLocation = mlc.getBoolean(new String[]{"useeyelocation", "uel"}, false);
-        this.lockPitch = mlc.getBoolean(new String[]{"lockpitch"}, false);
-        this.yOffset = mlc.getPlaceholderDouble(new String[]{"yoffset", "y"}, 0.0D);
-        this.xOffset = mlc.getPlaceholderDouble(new String[]{"xoffset", "x"}, 0.0D);
-        this.zOffset = mlc.getPlaceholderDouble(new String[]{"zoffset", "z"}, 0.0D);
+        this.forward = mlc.getPlaceholderDouble(new String[] {"forward", "f", "amount", "a"}, 5.0D);
+        this.rotate = mlc.getPlaceholderFloat(new String[] {"rotate", "rot"}, 0.0F);
+        this.useEyeLocation = mlc.getBoolean(new String[] {"useeyelocation", "uel"}, false);
+        this.lockPitch = mlc.getBoolean(new String[] {"lockpitch"}, false);
+        this.yOffset = mlc.getPlaceholderDouble(new String[] {"yoffset", "y"}, 0.0D);
+        this.xOffset = mlc.getPlaceholderDouble(new String[] {"xoffset", "x"}, 0.0D);
+        this.zOffset = mlc.getPlaceholderDouble(new String[] {"zoffset", "z"}, 0.0D);
     }
 
     @Override
